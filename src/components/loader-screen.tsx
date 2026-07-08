917fcd58de13dc193d9931cc1a5f1d467d871d3b
Tullner Bence <tlrbence@gmail.com>
2026-07-09 01:54:45 +0200

    fix: typo

--- src/components/loader-screen.tsx
+++ src/components/loader-screen.tsx
@@ -2,12 +2,12 @@ import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default function LoaderScreen() {
     return (
-        <View style={stlyes.container}>
+        <View style={styles.container}>
             <ActivityIndicator size="large" color="#000000" />
         </View>
     );
 }
 
-const stlyes = StyleSheet.create({
+const styles = StyleSheet.create({
     container: { flex: 1, alignItems: "center", justifyContent: "center" },
 });
