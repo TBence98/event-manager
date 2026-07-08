d1c90ccf0c67403b564a331c0dd16be35ec07de1
Tullner Bence <tlrbence@gmail.com>
2026-07-09 00:16:05 +0200

    fix: file name convention

--- src/app/events/index.tsx
+++ src/app/events/index.tsx
@@ -1,4 +1,4 @@
-import EventCard from "@/components/EventCard";
+import EventCard from "@/components/event-card";
 import LoaderScreen from "@/components/loader-screen";
 import { Screen } from "@/components/screen";
 import { useEvents } from "@/store/events";
